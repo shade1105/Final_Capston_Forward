89d7df87ca82aee2cadfe06853909e197973623e
LEEjaeHyun <swaa23@gmail.com>
2020-09-08 11:01:17 +0900

    출결 알고리즘

--- angular-flask/src/app/app-routing.module.ts
+++ angular-flask/src/app/app-routing.module.ts
@@ -1,30 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { NavbarComponent } from './components/navbar/navbar.component'
-import { HomeComponent } from './components/home/home.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { SigninComponent } from './components/signin/signin.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { SigninComponent } from "./components/signin/signin.component";
 import { ActionCamComponent } from "./components/action-cam/action-cam.component";
-import { RequestAttentionComponent } from './components/request-attention/request-attention.component';
-import { AttentionAdminComponent } from './components/attention-admin/attention-admin.component';
-import { AttentionStuComponent } from './components/attention-stu/attention-stu.component';
-import { PictureIndexComponent } from './components/picture-index/picture-index.component';
+import { RequestAttentionComponent } from "./components/request-attention/request-attention.component";
+import { AttentionAdminComponent } from "./components/attention-admin/attention-admin.component";
+import { AttentionStuComponent } from "./components/attention-stu/attention-stu.component";
+import { PictureIndexComponent } from "./components/picture-index/picture-index.component";
+import { MkSubjectComponent } from "./components/mk-subject/mk-subject.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "navbar", component: NavbarComponent },
   { path: "signup", component: SignupComponent },
-  { path: "signin", component: SigninComponent},
-  { path: "req-atten", component: RequestAttentionComponent},
-  { path: "action-cam", component: ActionCamComponent},
-  { path: "attention-admin", component: AttentionAdminComponent},
-  { path: "attention-stu", component: AttentionStuComponent},
-  { path: "picture-index", component: PictureIndexComponent}
+  { path: "signin", component: SigninComponent },
+  { path: "req-atten", component: RequestAttentionComponent },
+  { path: "action-cam", component: ActionCamComponent },
+  { path: "attention-admin", component: AttentionAdminComponent },
+  { path: "attention-stu", component: AttentionStuComponent },
+  { path: "picture-index", component: PictureIndexComponent },
+  { path: "mk-subject", component: MkSubjectComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
