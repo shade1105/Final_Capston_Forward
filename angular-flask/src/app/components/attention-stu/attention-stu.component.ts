1e4bbd450ece8f432be52b82df6bf50e9269ac53
LEEjaeHyun <swaa23@gmail.com>
2020-09-21 17:45:16 +0900

    stu_atten_date update

--- angular-flask/src/app/components/attention-stu/attention-stu.component.ts
+++ angular-flask/src/app/components/attention-stu/attention-stu.component.ts
@@ -17,9 +17,15 @@ export class AttentionStuComponent implements OnInit {
   fullImagePath: string;
   converted_image: string;
   Attentionlist = [];
-
-  ngOnInit() {
-    var msg;
+ ngOnInit() {
+  /*
+  ** 생성자가 호출될 때 배열에대한 날짜 포멧에 따라
+  ** 초기화해서 생성해줌  
+  ** input : null
+  ** return : null 
+  ** author  : 이재현
+  */
+  var msg;
     this.authService
       .getSubjectData(localStorage.getItem("user"))
       .subscribe((data) => {
@@ -40,6 +46,12 @@ export class AttentionStuComponent implements OnInit {
   }
 
   clicked(number) {
+    /*
+    ** 해당 버튼클릭시 number에 반환하는 값을 이미지형식으로 보여줌 
+    ** input : number(week)
+    ** return : image
+    ** author  : 이재현
+    */
     var asd;
     var dsd;
     var usernum;
@@ -60,6 +72,13 @@ export class AttentionStuComponent implements OnInit {
 
   clickevent(number) {
     //현재 날짜 체크해서 출석 가능/불가능 확인 알고리즘 추가
+    /*
+    ** 해당 버튼클릭시 number에 반환하는 값에 action-cam 을 실행하여
+    ** 출석 로직을 실행 
+    ** input : number(week)
+    ** return : null 
+    ** author  : 이재현
+    */
     var dsd;
     var usernum;
     var asd;
@@ -68,11 +87,6 @@ export class AttentionStuComponent implements OnInit {
     asd = this.Attentionlist[number - 1].week;
     this.authService.postAttendData(asd, usernum).subscribe();
     localStorage.setItem("number", number);
-    /**
-     ** 단순히 네비게이트 로 이동을 한 로직임 . 
-     ** 변수 값도 같이 옮길수 있는 방법이 있을까? 
-     **/
-
     this.router.navigate(["action-cam"]);
   }
 
